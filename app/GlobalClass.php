9d333408f024e971d94aef07e6d79164c8d57631
chhaattbackend <chhattmediapvt@gmail.com>
2021-07-06 12:16:37 +0500

    images

--- app/GlobalClass.php
+++ app/GlobalClass.php
@@ -3,6 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+
 
 class GlobalClass extends Model
 {
@@ -12,11 +15,25 @@ class GlobalClass extends Model
 
     public function storeS3($file, $path)
     {
-
         $exe = $file->getClientOriginalName();
         $filename = time() . '-' . $exe;
-        $file->storeAs($path, $filename, 's3');
-       
+        $destinationPath = public_path('/images/nikal');
+        $file->move($destinationPath,$filename);
+        $first = $destinationPath.'/'.$filename;
+
+        $img = Image::make($destinationPath . '/' . $filename);
+        $img->insert(public_path('images/watermark.png'), 'center');
+        $img->save($destinationPath . '/' . 'second' . $filename);
+        $second = $destinationPath . '/' . 'second' . $filename;
+
+        $inserted = Image::make($destinationPath . '/' . 'second' . $filename);
+        $image = Image::make($inserted)->resize(400, 300)->encode('png');
+
+        $path1 = Storage::disk('s3')->put($path.'/'.$filename , (string)$image);
+
+        @unlink($first);
+        @unlink($second);
+        // $file->storeAs($path, $filename, 's3');
         return $filename;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\StoreProductController;
 use App\StoreProduct;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +29,29 @@ Route::get('/home', function () {
 
     return redirect('/acategories');
 });
+
+
+// Route::get('addWatermark', function()
+// {
+
+//     $img = Image::make(public_path('images/123.PNG'));
+
+//     /* insert watermark at bottom-right corner with 10px offset */
+//     $img->insert(public_path('images/1613475579.JPG'), 'bottom-right', 10, 10);
+
+//     $img->save(public_path('images/123123123123.png'));
+
+//     //  $img_new = file_get_contents('images/123123123123.png');
+//      $file= Image::make(public_path('images/123123123123.PNG'));
+
+
+
+//     //  $img_new->storeAs('construction', 'hello.png', 's3');
+//     $image = Image::make($file)->resize(400,300)->encode('png');
+//     $path = Storage::disk('s3')->put('construction/', (string)$image);
+//     //$s3->put('construction/', file_get_contents($img_new));
+//     dd('saved image successfully.');
+// });
 
 Route::group(['middleware' => ['auth']], function () {
     // Route::get('/ag', 'StoreProductController@index2')->name('ag');
