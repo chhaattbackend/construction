22df699a76248ab9d84d5b17d200b0fd699e29fb
chhaattbackend <chhattmediapvt@gmail.com>
2021-07-26 17:42:47 +0500

    ok

--- resources/views/admin/user_service/edit.blade.php
+++ resources/views/admin/user_service/edit.blade.php
@@ -14,7 +14,7 @@
                 <!-- Horizontal Form -->
                 <div class="card card-info">
                     <div class="card-header">
-                        <h3 class="card-title">Add User Service</h3>
+                        <h3 class="card-title">Edit User Service</h3>
                     </div>
                     <!-- /.card-header -->
                     <!-- form start -->
